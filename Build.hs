b819fc009aa840e0cdd9f0bf4800cac62195da52
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-11-30 15:48:37 -0500

    experiment

---
+++ Build.hs
@@ -0,0 +1,13 @@
+import Development.Shake
+import Development.Shake.Command
+import Development.Shake.FilePath
+import Development.Shake.Util
+
+
+main :: IO ()
+main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
+    -- I've also tried shakeFiles="_shake"; same result
+  want ["_build/test.txt"]
+
+  "_build/test.txt" %> \_ -> do
+    shell "echo testing" ">> _build/test.txt"
